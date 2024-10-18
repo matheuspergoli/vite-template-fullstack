267bd821b6b29b9fe2b18c50fb929aaf3f84e67b
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2024-10-18 20:21:22 -0300

    chore: update start command and packages

--- src/environment/server.ts
+++ src/environment/server.ts
@@ -3,7 +3,7 @@ import { z } from "zod"
 
 export const serverEnv = createEnv({
 	server: {
-		NODE_ENV: z.enum(["development", "production", "test"]).default("development")
+		NODE_ENV: z.enum(["development", "production", "test"])
 	},
 	runtimeEnv: process.env,
 	emptyStringAsUndefined: true
