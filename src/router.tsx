44819c9351bf5d61bb789672ac311ba60f14a21f
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2024-11-10 04:48:56 -0300

    chore: remove progress component

--- src/router.tsx
+++ src/router.tsx
@@ -2,14 +2,12 @@ import { createRouter, ErrorComponent } from "@tanstack/react-router"
 
 import { api, queryClient, trpcQueryUtils } from "./libs/trpc"
 import { routeTree } from "./routeTree.gen"
-import { LoadingProgress } from "./shared/ui/loading-progress"
 
 export const router = createRouter({
 	routeTree,
 	defaultStaleTime: 0,
 	defaultPreload: "intent",
 	context: { api, queryClient, trpcQueryUtils },
-	defaultPendingComponent: () => <LoadingProgress />,
 	defaultErrorComponent: ({ error }) => <ErrorComponent error={error} />,
 	defaultNotFoundComponent: () => <p className="p-2 text-2xl">Not Found</p>
 })
