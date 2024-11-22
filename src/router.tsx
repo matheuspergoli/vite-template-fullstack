22dabee4f9216d5e725b074fe25c340e562859aa
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2024-11-22 05:20:56 -0300

    fix: add stale time to always call the loaders

--- src/router.tsx
+++ src/router.tsx
@@ -5,9 +5,10 @@ import { routeTree } from "./routeTree.gen"
 
 export const router = createRouter({
 	routeTree,
-	defaultStaleTime: 0,
 	defaultPreload: "intent",
+	defaultPreloadStaleTime: 0,
 	context: { api, queryClient, trpcQueryUtils },
 	defaultErrorComponent: ({ error }) => <ErrorComponent error={error} />,
+	defaultPendingComponent: () => <p className="p-2 text-2xl">Loading...</p>,
 	defaultNotFoundComponent: () => <p className="p-2 text-2xl">Not Found</p>
 })
