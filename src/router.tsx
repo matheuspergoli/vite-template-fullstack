0b1f39c50ef708a1c83206ba59c2fb00eca42999
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2024-12-16 12:19:23 -0300

    update to react19

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
 import { createRouter } from "@tanstack/react-router"
 
-import { queryClient, trpc, trpcQueryUtils } from "./libs/trpc"
+import { queryClient, trpcQueryUtils } from "./libs/trpc"
 import { routeTree } from "./routeTree.gen"
 import { DefaultCatchBoundary } from "./shared/components/default-catch-boundary"
 import { DefaultNotFound } from "./shared/components/default-not-found"
@@ -9,7 +9,7 @@ export const router = createRouter({
 	routeTree,
 	defaultPreload: "intent",
 	defaultPreloadStaleTime: 0,
-	context: { trpc, queryClient, trpcQueryUtils },
+	context: { queryClient, trpcQueryUtils },
 	defaultNotFoundComponent: () => <DefaultNotFound />,
 	defaultErrorComponent: (error) => <DefaultCatchBoundary {...error} />,
 	defaultPendingComponent: () => <p className="p-2 text-2xl">Loading...</p>
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,10 +4,9 @@ import {
 	ScrollRestoration
 } from "@tanstack/react-router"
 
-import { queryClient, trpc, trpcQueryUtils } from "@/libs/trpc"
+import { queryClient, trpcQueryUtils } from "@/libs/trpc"
 
 export const Route = createRootRouteWithContext<{
-	trpc: typeof trpc
 	queryClient: typeof queryClient
 	trpcQueryUtils: typeof trpcQueryUtils
 }>()({
